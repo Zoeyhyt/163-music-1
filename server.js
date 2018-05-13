e7f8fe488f7a4fcfa53c10413a8dae82a669289f
Zoey H <zoeyh@ZoeydeMacBook-Pro.local>
2018-05-14 00:15:15 +0800

    上传第一首歌

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http')
 var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
+var qiniu = require('qiniu')
 
 if(!port){
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
@@ -21,12 +22,29 @@ var server = http.createServer(function(request, response){
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path === '/'){
-    let string = fs.readFileSync('./index.html','uft-8')
+  if(path === '/uptoken'){
     response.statusCode = 200
-    var accessKey = 'your access key';
-    var secretKey = 'your secret key';
+    response.setHeader('Content-Type','text/json;charset=utf-8')
+    response.setHeader('Access-Control-Allow-Origin','*')
+    response.removeHeader('Date')
+
+    var config = fs.readFileSync('./src/qiniu-key.json')
+    config = JSON.parse(config)
+
+    let {accessKey,secretKey} = config;
     var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+    var options = {
+      scope: '163-music-1',
+    };
+    var putPolicy = new qiniu.rs.PutPolicy(options);
+    var uploadToken=putPolicy.uploadToken(mac);
+
+    response.write(`
+    {
+      "uptoken":"${uploadToken}"
+    }
+    `)
+
     response.end()
   }else{
     response.statusCode = 404
